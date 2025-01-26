26c370628ea2a7358c7c8b252d9d4675a6084ab1
saicharan293 <2493saicharan@gmail.com>
2025-01-27 01:32:22 +0530

    added logs to check the issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,25 +5,24 @@ const googleAuth = passport.authenticate("google", { scope: ["profile", "email"]
 
 //working source code
 const googleCallback = (req, res, next) => {
-  console.log("Inside Google Callback");
+  console.log("Google callback route reached"); // Add this
   passport.authenticate("google", (err, user, info) => {
-    console.log("Passport Authenticate Callback Invoked");
+    console.log("Inside passport.authenticate"); // Add this
     if (err || !user) {
       console.error("Authentication failed:", err || "No user found");
       return res.redirect("/"); // Redirect to failure page
     }
-
     req.logIn(user, (loginErr) => {
       if (loginErr) {
         console.error("Login error:", loginErr);
         return next(loginErr);
       }
-
-      console.log("Login successful. Redirecting to /platform");
+      console.log("Login successful. Redirecting to /platform"); // Add this
       return res.redirect(`/platform`);
     });
   })(req, res, next);
 };
+
 
 const logout = (req, res) => {
   req.logout(() => {
