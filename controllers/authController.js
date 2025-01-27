ad127cc6241742a6cc787c8f109a2c398de8d9db
saicharan293 <2493saicharan@gmail.com>
2025-01-27 16:00:40 +0530

    second trail on google SSO

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,12 @@
 const passport = require("passport");
 require("../config/passportConfig");
 
-const googleAuth = passport.authenticate("google", { scope: ["profile", "email"], prompt: "select_account" });
+// const googleAuth = passport.authenticate("google", { scope: ["profile", "email"], prompt: "select_account" });
+const googleAuth = (req, res, next) => {
+  const isNewUser = req.query.isNew === "true"; // Check if it's a new user flow
+  const prompt = isNewUser ? "consent" : "select_account";
+  passport.authenticate("google", { scope: ["profile", "email"], prompt })(req, res, next);
+};
 
 //working source code
 const googleCallback = (req, res, next) => {
